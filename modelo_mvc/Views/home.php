e113217f7bce079f0c48ecbde611b8ff9359ca4c
eneasdev5 <eneasdev@gmail.com>
2024-02-20 21:41:31 -0300

    projeto mvc

---
+++ modelo_mvc/Views/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modelo MVC</title>
+</head>
+
+<body>
+    <h1>PHP COM MVC</h1>
+</body>
+
+</html>
--- php-web/index.php
+++ php-web/index.php
@@ -2,24 +2,24 @@
 
 use Src\Models\User;
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 
 
 $newUser = new stdClass;
-$newUser->first_name="Letícia Isis da Mata";
-$newUser->last_name="Mata";
-$newUser->phone="(17) 3685-3015";
-$newUser->user_type="client";
-$newUser->login="leciciamata123";
-$newUser->password= "leciciamata123";
+$newUser->first_name = "Letícia Isis da Mata";
+$newUser->last_name = "Mata";
+$newUser->phone = "(17) 3685-3015";
+$newUser->user_type = "client";
+$newUser->login = "leciciamata123";
+$newUser->password = "leciciamata123";
 
 // $objectUser = (new User)->register_client($newUser);
 // if(!$objectUser->fail()) {
 //   var_dump($objectUser->data());
 // }
 
-$objectUser = (new User)->findById(4);
+// $objectUser = (new User)->findById(4);
 // $objectUser->login = $newUser->login;
 // $objectUser->password = password_hash($newUser->login, 
 //   PASSWORD_DEFAULT, [10]);
